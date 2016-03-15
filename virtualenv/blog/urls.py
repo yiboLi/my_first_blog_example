0b885ae08616e5560d63d68cf459dd5717e47526
yinoLi <irisfulyiboli@gmail.com>
2016-03-15 16:05:08 -0400

    Added view and template for detailed post & CSS for the site

--- virtualenv/blog/urls.py
+++ virtualenv/blog/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views #import blog/views.py
 
 urlpatterns = [
+    url('^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),# Django will take everything that you place here and transfer it to a view as a variable called pk. 
     url(r'^$', views.post_list, name='post_list'), # an empty string will match because http://127.0.0.1:8000/ is not a part of URL.
 ] # This pattern will tell Django that views.post_list is the right place to go if someone enters your website at the 'http://127.0.0.1:8000/' address.
   # name='post_list' is the name of the URL that will be used to identify the view.
--- virtualenv/blog/views.py
+++ virtualenv/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -6,3 +6,7 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts':posts}) #add posts for the template to use,'posts' is the name
+
+def post_detail(request, pk): #pk is pass-in parameter from urls.py
+    post = get_object_or_404(Post, pk=pk) #get only one post
+    return render(request, 'blog/post_detail.html', {'post':post}) #pass-in post for template to use
