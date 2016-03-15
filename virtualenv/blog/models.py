b96b8c036cf96a526cfb345e31613ea8599ac30f
yinoLi <irisfulyiboli@gmail.com>
2016-03-15 12:30:56 -0400

    Modified templates to display posts from database

--- virtualenv/blog/models.py
+++ virtualenv/blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 # Create your models here.
 class Post(models.Model):                     #the class Post is Django model
-    auther = models.ForeignKey('auth.User')    # a link to another model
+    author = models.ForeignKey('auth.User')    # a link to another model
     title = models.CharField(max_length=200)   #define text with a limited number of characters
     text = models.TextField()                  #long text without a limit
     created_date = models.DateTimeField(default=timezone.now)
